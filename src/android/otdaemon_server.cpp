2231d0ca6c89e63c787efe8eda65e94dae69b10b
Kangping Dong <wgtdkp@google.com>
2024-03-07 20:54:06 +0800

    [Thread] support customizing meshcop TXT entries
    Bug: 311324956
    Test: atest ThreadNetworkIntegrationTests:android.net.thread.ServiceDiscoveryTest#meshcopOverlay_vendorAndModelNameAreSetToOverlayValue
    Change-Id: I2a50d64a35d975155f8db4657c5db7edf6f22628

--- src/android/otdaemon_server.cpp
+++ src/android/otdaemon_server.cpp
@@ -116,7 +116,8 @@ static const char *ThreadEnabledStateToString(int enabledState)
 }
 
 OtDaemonServer::OtDaemonServer(Application &aApplication)
-    : mNcp(aApplication.GetNcp())
+    : mApplication(aApplication)
+    , mNcp(aApplication.GetNcp())
     , mBorderAgent(aApplication.GetBorderAgent())
     , mMdnsPublisher(static_cast<MdnsPublisher &>(aApplication.GetPublisher()))
     , mBorderRouterConfiguration()
@@ -150,6 +151,10 @@ void OtDaemonServer::BinderDeathCallback(void *aBinderServer)
     OtDaemonServer *thisServer = static_cast<OtDaemonServer *>(aBinderServer);
 
     otbrLogCrit("Client is died, removing callbacks...");
+
+    // Note that the INsdPublisher reference is hold in MdnsPublisher
+    thisServer->mMdnsPublisher.SetINsdPublisher(nullptr);
+
     thisServer->mCallback = nullptr;
     thisServer->mTunFd.set(-1); // the original FD will be closed automatically
 }
@@ -373,7 +378,8 @@ void OtDaemonServer::Process(const MainloopContext &aMainloop)
 
 Status OtDaemonServer::initialize(const ScopedFileDescriptor           &aTunFd,
                                   const bool                            enabled,
-                                  const std::shared_ptr<INsdPublisher> &aINsdPublisher)
+                                  const std::shared_ptr<INsdPublisher> &aINsdPublisher,
+                                  const MeshcopTxtAttributes           &aMeshcopTxts)
 {
     otbrLogInfo("OT daemon is initialized by system server (tunFd=%d, enabled=%s)", aTunFd.get(),
                 enabled ? "true" : "false");
@@ -382,14 +388,21 @@ Status OtDaemonServer::initialize(const ScopedFileDescriptor           &aTunFd,
     // we can process `aTunFd` directly in front of the task.
     mTunFd = aTunFd.dup();
 
-    mTaskRunner.Post([enabled, aINsdPublisher, this]() { initializeInternal(enabled, aINsdPublisher); });
+    mTaskRunner.Post(
+        [enabled, aINsdPublisher, aMeshcopTxts, this]() { initializeInternal(enabled, aINsdPublisher, aMeshcopTxts); });
 
     return Status::ok();
 }
 
-void OtDaemonServer::initializeInternal(const bool enabled, const std::shared_ptr<INsdPublisher> &aINsdPublisher)
+void OtDaemonServer::initializeInternal(const bool                            enabled,
+                                        const std::shared_ptr<INsdPublisher> &aINsdPublisher,
+                                        const MeshcopTxtAttributes           &aMeshcopTxts)
 {
-    mINsdPublisher = aINsdPublisher;
+    mMdnsPublisher.SetINsdPublisher(aINsdPublisher);
+
+    std::string instanceName = aMeshcopTxts.vendorName + " " + aMeshcopTxts.modelName;
+    mBorderAgent.SetMeshcopServiceValues(instanceName, aMeshcopTxts.modelName, aMeshcopTxts.vendorName,
+                                         aMeshcopTxts.vendorOui);
 
     if (enabled)
     {
@@ -403,27 +416,26 @@ void OtDaemonServer::initializeInternal(const bool enabled, const std::shared_pt
 
 void OtDaemonServer::updateThreadEnabledState(const int enabled, const std::shared_ptr<IOtStatusReceiver> &aReceiver)
 {
-    if (mThreadEnabled != enabled)
-    {
-        otbrLogInfo("Thread enabled state changed: %s -> %s", ThreadEnabledStateToString(mThreadEnabled),
-                    ThreadEnabledStateToString(enabled));
-        mThreadEnabled = enabled;
-    }
+    VerifyOrExit(enabled != mThreadEnabled);
+
+    otbrLogInfo("Thread enabled state changed: %s -> %s", ThreadEnabledStateToString(mThreadEnabled),
+                ThreadEnabledStateToString(enabled));
+    mThreadEnabled = enabled;
 
     if (aReceiver != nullptr)
     {
         aReceiver->onSuccess();
     }
 
+    // Enables the BorderAgent module only when Thread is enabled because it always
+    // publishes the MeshCoP service even when no Thread network is provisioned.
     switch (enabled)
     {
     case OT_STATE_ENABLED:
-        mMdnsPublisher.SetINsdPublisher(mINsdPublisher);
+        mBorderAgent.SetEnabled(true);
         break;
     case OT_STATE_DISABLED:
-        mMdnsPublisher.SetINsdPublisher(nullptr);
-        break;
-    default:
+        mBorderAgent.SetEnabled(false);
         break;
     }
 
@@ -431,6 +443,9 @@ void OtDaemonServer::updateThreadEnabledState(const int enabled, const std::shar
     {
         mCallback->onThreadEnabledChanged(mThreadEnabled);
     }
+
+exit:
+    return;
 }
 
 void OtDaemonServer::enableThread(const std::shared_ptr<IOtStatusReceiver> &aReceiver)
